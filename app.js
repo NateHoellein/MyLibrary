b44808d367e2183650631d8f3b72da056509a213
Nate Hoellein <natehoellein@gmail.com>
2012-11-22 10:24:00 -0500

    Added a controller to remove dependancy of library on app.js Added unit tests for the controller

--- app.js
+++ app.js
@@ -1,92 +1,85 @@
-
 var express = require('express')
-  , routes = require('./routes')
-  , library = require('./lib/mylibrary')
-  , response = require('./lib/bookDefs')
-  , isbnDb = require('./lib/isbndb')
-  , check = require('validator').check
-  , sanitize = require('validator').sanitize
+    , routes = require('./routes')
+    , library = require('./lib/mylibrary')
+    , response = require('./lib/bookDefs')
+    , isbnDb = require('./lib/isbndb')
+    , appController = require('./lib/AppController')
+    , check = require('validator').check
+    , sanitize = require('validator').sanitize
+    , controller;
 
 var app = module.exports = express.createServer();
 // Configuration
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+app.configure(function () {
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
 
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-  library.init('localhost/book_test')
+app.configure('development', function () {
+    app.use(express.errorHandler({ dumpExceptions:true, showStack:true }));
+    library.init('localhost/book_test');
+    controller = appController(library);
 });
 
-app.configure('production', function(){
-  app.use(express.errorHandler()); 
+app.configure('production', function () {
+    app.use(express.errorHandler());
 });
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.sendfile(__dirname + '/public/Main.html')
 });
 
-app.get('/add', function(req, res) {
+app.get('/add', function (req, res) {
     res.sendfile(__dirname + '/public/Add.html');
 });
 
-app.get('/search', function(req, res){
+app.get('/search', function (req, res) {
     res.sendfile(__dirname + '/public/Search.html')
 });
 
-app.get('/library', function(req, res){
-    library.all(function(response){
+app.get('/library', function (req, res) {
+    library.all(function (response) {
         (response.Status === 0) ? res.json(response.Books, 200) : res.json(response.Message, 409);
     })
 })
 
-app.get('/library/loanedOut', function(req, res){
-    console.log('loanedout called');
-
-    library.loanedOut(function(response){
+app.get('/library/loanedOut', function (req, res) {
+    controller.loanedOut(function (response) {
         (response.Status === 0) ? res.json(response.Books, 200) : res.json(response.Message, 409);
     })
 })
 
-app.post('/library/search',function(req, res){
+app.post('/library/search', function (req, res) {
     var character = req.body.character;
     var cleanCharacter = sanitize(character).xss();
-    library.search(cleanCharacter, function(response) {
+    library.search(cleanCharacter, function (response) {
         (response.Status === 0) ? res.json(response.Books, 200) : res.json(response.Message, 409);
     })
 });
 
-app.post('/add', function(req, res){
-    var newBook = req.body;
-    library.add(newBook, function(response){
+app.post('/add', function (req, res) {
+
+    controller.add(req.body, function (response) {
         (response.Status === 0) ? res.json("OK", 200) : res.json(response.Message, 409);
     })
 });
 
-app.post('/library/isbn', function(req, res){
+app.post('/library/isbn', function (req, res) {
     var isbn = req.body.isbn;
     var cleanIsbn = sanitize(isbn).xss();
-    isbnDb.bookInfo(cleanIsbn, function(bookInfo){
+    isbnDb.bookInfo(cleanIsbn, function (bookInfo) {
         (bookInfo.Status === 0) ? res.json(bookInfo, 200) : res.json(response.Message, 409);
     });
 });
 
-app.post('/library/update', function(req, res) {
-    var book = {
-        Title: req.body.Title,
-        Location: req.body.Location,
-        LoanedOut: req.body.LoanedOut
-    };
-    console.log(book);
-    library.update(book, function(response) {
-        console.log(response);
+app.post('/library/update', function (req, res) {
+    controller.update(req.body, function (response) {
         (response.Status === 0) ? res.json("OK", 200) : res.json(response.Message, 409);
     })
 })
---
+++ spec/AppControllerSpec.js
@@ -0,0 +1,193 @@
+var should = require('should')
+    , mocha = require('mocha')
+    , appController = require('../lib/AppController')
+
+describe("AppController", function () {
+
+    it("updates books by calling library functions", function (done) {
+        var testObject = {
+
+            update:function (book, callback) {
+
+                book.Title.should.equal("Blue moon");
+                book.Location.should.equal("silver bar");
+                book.LoanedOut.should.be.true;
+
+                var message = {
+                    status:0,
+                    message:"snorkle"
+                };
+                callback(message);
+            }};
+
+
+        var controller = appController(testObject);
+
+        var body = {
+            Title:"Blue moon",
+            Location:"silver bar",
+            LoanedOut:true
+        };
+        controller.update(body, function (message) {
+
+            message.status.should.equal(0);
+            message.message.should.equal("snorkle");
+            done();
+        });
+
+    });
+
+    it('finds all loaned out books.', function (done) {
+        var testObject = {
+            loanedOut:function (callback) {
+                var message = {
+                    status:0,
+                    message:"OK",
+                    books:[
+                        {
+                            Title:"My book",
+                            Location:"Reilly",
+                            LoanedOut:true,
+                            ISBN:"1234"
+                        }
+                    ]
+                }
+                callback(message);
+            }
+        };
+
+        var controller = appController(testObject);
+        controller.loanedOut(function (message) {
+            message.message.should.equal("OK");
+            message.status.should.equal(0);
+            done();
+        })
+    });
+
+    it('finds all loaned out books and unencodes the "$".', function (done) {
+        var testObject = {
+            loanedOut:function (callback) {
+                var message = {
+                    status:0,
+                    message:"OK",
+                    books:[
+                        {
+                            Title:"My book has some $ $ signs.",
+                            Location:"Reilly",
+                            LoanedOut:true,
+                            ISBN:"1234"
+                        }
+                    ]
+                }
+                callback(message);
+            }
+        };
+
+        var controller = appController(testObject);
+        controller.loanedOut(function (message) {
+            message.message.should.equal("OK");
+            message.status.should.equal(0);
+            message.books[0].Title.should.equal("My book has some $ $ signs.");
+            done();
+        })
+    });
+
+    it('adds a new book.', function (done) {
+        var testObject = {
+            add:function (book, callback) {
+                book.Title.should.equal("my new book");
+                book.Subject.should.equal("subject about something");
+                book.Author.should.equal('Bill Jones');
+                book.ISBN.should.equal("1234567890");
+                book.Location.should.equal("Nate");
+                book.LoanedOut.should.be.true;
+
+                var message = {
+                    status:0,
+                    message:"OK",
+                    books:[]
+                }
+                callback(message);
+            }
+        }
+
+        var controller = appController(testObject);
+
+        var body = {
+            Title:"my new book",
+            Subject:"subject about something",
+            Author:"Bill Jones",
+            ISBN:"1234567890",
+            Location:"Nate",
+            LoanedOut:"true"
+        }
+        controller.add(body, function (message) {
+            message.status.should.equal(0);
+            message.message.should.equal("OK");
+            message.books.should.eql([]);
+            done();
+        });
+
+    });
+
+    it('adds a new book sanitized for xss', function (done) {
+        var testObject = {
+            add:function (book, callback) {
+                book.Title.should.equal("my new [removed]alert&#40;'Hi'&#41;[removed] book");
+                book.Subject.should.equal("subject about $ something");
+                book.Author.should.equal('Bill Jones');
+                book.ISBN.should.equal("1234567890");
+                book.Location.should.equal("Nate[removed]function(){}[removed]");
+                book.LoanedOut.should.be.true;
+
+                var message = {
+                    status:0,
+                    message:"OK",
+                    books:[]
+                }
+                callback(message);
+            }
+        }
+
+        var controller = appController(testObject);
+
+        var body = {
+            Title:"my new <script>alert('Hi')</script> book",
+            Subject:"subject about $ something",
+            Author:"Bill Jones",
+            ISBN:"1234567890",
+            Location:"Nate<script type='a'>function(){}</script>",
+            LoanedOut:"true"
+        }
+        controller.add(body, function (message) {
+            message.status.should.equal(0);
+            message.message.should.equal("OK");
+            message.books.should.eql([]);
+            done();
+        });
+
+    });
+
+    it('Returns all books.', function (done) {
+        var testObject = {
+            all:function (callback) {
+                var message = {
+                    status:0,
+                    message:"OK",
+                    books:[]
+                }
+                callback(message);
+            }
+        }
+
+        var controller = appController(testObject);
+
+        controller.all(function (message) {
+            message.status.should.equal(0);
+            message.message.should.equal("OK");
+            message.books.should.eql([]);
+            done();
+        });
+
+    });
+});
